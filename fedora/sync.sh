222ec5b99f703de80fc4d4145b1736f016f9cb85
Ben Cordero (bencord0) <bencord0@condi.me>
2014-05-05 03:58:52 +0100

    Add fedora packages

--- fedora/sync.sh
+++ fedora/sync.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 CURRENT_DIR=$(dirname $0)
+BASEURL=rsync://rsync.mirrorservice.org/dl.fedoraproject.org/pub/fedora/linux/
+
+# Stable release and install media
+mkdir -p "${CURRENT_DIR}/releases/20/Fedora/x86_64/os"
 rsync -vaH --numeric-ids --delete --delete-after --delay-updates \
-    "rsync://dl.fedoraproject.org/fedora-enchilada/linux/releases/20/Fedora/x86_64/os" \
-    "${CURRENT_DIR}/releases/20/Fedora/x86_64"
+         "${BASEURL}releases/20/Fedora/x86_64/os/" \
+    "${CURRENT_DIR}/releases/20/Fedora/x86_64/os"
+
+# Packages
+mkdir -p "${CURRENT_DIR}/releases/20/Everything/x86_64/os/Packages"
+rsync -vaH --numeric-ids --delete --delete-after --delay-updates \
+         "${BASEURL}releases/20/Everything/x86_64/os/Packages/" \
+    "${CURRENT_DIR}/releases/20/Everything/x86_64/os/Packages"
